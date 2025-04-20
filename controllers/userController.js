3a1cf83bb49e01c4832a4342e7469980ffd2a0fb
Héctor Enríquez <hectoren@gmail.com>
2025-04-19 22:41:51 -0500

    Se crea el método que regresa todos los usuarios de tipo Profesor

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 // backend/controllers/userController.js
-
+const UserType = require('../models/UserType');  // Ajusta la ruta si es necesario
 const User = require('../models/User');
 
 exports.getAllUsers = async (req, res) => {
@@ -50,3 +50,26 @@ exports.deleteUserByUsername = async (req, res) => {
     res.status(500).json({ message: 'Error al eliminar usuario' });
   }
 };
+
+// Obtener todos los usuarios con tipo 'Profesor'
+exports.getAllProfesores = async (req, res) => {
+  try {
+    // Obtener el tipo de usuario "Profesor" desde la colección `usertypes`
+    const tipoProfesor = await UserType.findOne({ type: 'Profesor' });
+
+    if (!tipoProfesor) {
+      return res.status(404).json({ message: 'Tipo de usuario "Profesor" no encontrado' });
+    }
+
+    // Obtener todos los usuarios que tengan el tipo "Profesor" asociado
+    const profesores = await User.find({ tipoUsuario: tipoProfesor._id }).populate('tipoUsuario');
+
+    res.json(profesores);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error al obtener los profesores' });
+  }
+};
+
+
+
--- routes/user.js
+++ routes/user.js
@@ -9,5 +9,6 @@ router.get('/getallusers/', authMiddleware, userController.getAllUsers);
 router.get('/getuserbyusername/:username', authMiddleware, userController.getUserByUsername);
 router.put('/updateuserbyusername/:username', authMiddleware, userController.updateUserByUsername);
 router.delete('/deleteuserbyusername/:username', authMiddleware, userController.deleteUserByUsername);
+router.get('/getallprofesores/', authMiddleware, userController.getAllProfesores);
 
 module.exports = router;
